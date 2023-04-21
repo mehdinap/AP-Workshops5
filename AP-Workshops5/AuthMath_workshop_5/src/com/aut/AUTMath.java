f61b3d0b6b8c1539d59245b9b0a8120e03ce1b0b
mehdinap <125260379+mehdinap@users.noreply.github.com>
2023-04-21 22:50:40 +0330

    fix the project

---
+++ AP-Workshops5/AuthMath_workshop_5/src/com/aut/AUTMath.java
@@ -0,0 +1,92 @@
+package com.aut;
+
+public class AUTMath {
+    /**
+     * gets two integers and returns their sum.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     * @return int
+     */
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    /**
+     * gets two integer and returns their subtraction.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     * @return int
+     */
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    /**
+     * gets two numbers and returns their subtraction.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     * @return int
+     */
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    /**
+     * gets two numbers and returns their division.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     * @return int
+     */
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    /**
+     * gets a number and returns its factorial.
+     *
+     * @param number the number
+     * @return int
+     */
+    public static int factorial(int number) {
+        int result = 1;
+
+        for (int counter = 1; counter <= number; counter++) {
+            result = multiply(result, counter);
+        }
+        return result;
+    }
+
+    /**
+     * gets a base and a power and calculates base in power of the input.
+     *
+     * @param base  the base
+     * @param power the power
+     * @return int
+     */
+    public static int pow(int base, int power) {
+        int result = 1;
+
+        for (int counter = 1; counter <= power; counter++) {
+            result = multiply(result, base);
+        }
+
+        return result;
+    }
+
+    /**
+     * gets an index and calculates the fibonacci sequence at that exact index recursively.
+     *
+     * @param index the index
+     * @return int
+     */
+    public static int fib(int index) {
+        if (index == 0 || index == 1) {
+            return index;
+        }
+        return fib(index - 1) + fib(index - 2);
+    }
+}
